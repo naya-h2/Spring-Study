f6e0e565c60f6589657e5580f95bb428c5a5405b
heewon <hw7666@sogang.ac.kr>
2024-07-26 14:11:40 +0900

    섹션3: 스프링으로 변환

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# Spring-Study
-스프링 공부중 ..🤔💦💫
+# Spring 공부 중 ..💭
+> 강의: [스프링 핵심 원리 - 기본편](https://www.inflearn.com/course/%EC%8A%A4%ED%94%84%EB%A7%81-%ED%95%B5%EC%8B%AC-%EC%9B%90%EB%A6%AC-%EA%B8%B0%EB%B3%B8%ED%8E%B8)
+
+## 📂 강의 기록
+- [객체 지향 설계와 스프링](https://www.notion.so/naya-h2/Spring-e0850ffdff254747881a6fa50ea1994d?p=26f051ba7389403e844ed24a0179ddc1&pm=s#beeb213ab9fe4b8cb4243a0a1ba0ae4e)
+- [스프링 핵심 원리 이해1 - 예제 만들기](https://naya-h2.notion.site/1-003b187380ea43e39d7634f42abd063a?pvs=4)
+  - [💻 실습코드](https://github.com/naya-h2/Spring-Study/tree/main/core)
+- [스프링 핵심 원리 이해2 - 객체 지향 원리 적용](https://naya-h2.notion.site/1-003b187380ea43e39d7634f42abd063a?pvs=4)
+  - [💻 실습코드](https://github.com/naya-h2/Spring-Study/tree/main/core)
--- core/src/main/java/hello/core/MemberApp.java
+++ core/src/main/java/hello/core/MemberApp.java
@@ -4,12 +4,17 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig(); //AppConfig 무조건 생성
-//        MemberService memberService = new MemberServiceImpl();
-        MemberService memberService = appConfig.memberService(); //구현체는 AppConfig에서 꺼내서 쓴다
+//        AppConfig appConfig = new AppConfig(); //AppConfig 무조건 생성
+//        MemberService memberService = appConfig.memberService(); //구현체는 AppConfig에서 꺼내서 쓴다
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class); //("찾고자 하는 객체 이름", 타입)
+
         Member member = new Member(1L, "heewon", Grade.VIP);
         memberService.join(member);
 
